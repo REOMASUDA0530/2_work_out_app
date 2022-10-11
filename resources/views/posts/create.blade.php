7d53c9a65c0b6404fa6491dc6a207ec03a806193
REOMASUDA0530 <qazsedc0530@gmail.com>
2022-10-12 01:26:05 +0900

    221012

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,18 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class='title'>
-        <h2>TITLE</h2>
-        <input type='text' name='post[title]' placeholder='TITLE'/>
-    </div>
-    <div class='body'>
-        <h2>BODY</h2>
-        <textarea name="post[body]" placeholder="BODY"></textarea>
-    </div>
-    <div class='tags'>
-        <p>tagを表示</p>
-    </div>
-    <input type="submit" value="SUBMIT"/>
+    <form action="/posts" method="POST">
+        @csrf
+        <div class='title'>
+            <h2>TITLE</h2>
+            <input type='text' name='post[title]' placeholder='TITLE'/>
+        </div>
+        <div class='body'>
+            <h2>BODY</h2>
+            <textarea name="post[body]" placeholder="BODY"></textarea>
+        </div>
+        <div class='tags'>
+            <p>tagを表示</p>
+            <input type="text" name='post[training_event_id]' value=1>
+        </div>
+        <div class='user_id'>
+            <input type='number' name='post[user_id]' value={{ Auth::id() }}>
+        </div>
+        <input type="submit" value="SUBMIT"/>
+    </form>
     <div class="back">
         <a href="/">BACK</a>
     </div>
