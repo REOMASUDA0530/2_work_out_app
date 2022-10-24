afd86c80f94dfb032fd9735cc1c4eb41ddefaad7
REOMASUDA0530 <qazsedc0530@gmail.com>
2022-10-24 23:19:32 +0900

    for heroku

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,11 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            usersTableSeeder::class,
             typesTableSeeder::class,
-            eventsTableSeeder::class,
-            postsTableSeeder::class,
-            event_postTableSeeder::class,
             
         ]);
     }
--- database/seeds/typesTableSeeder.php
+++ database/seeds/typesTableSeeder.php
@@ -12,10 +12,46 @@ class typesTableSeeder extends Seeder
     public function run()
     {
         $param = [
-            'name' => Str::random(5),
+            'name' => 'Bench press',
         
         ];
             
         DB::table('types')->insert($param);
+        
+        $param = [
+            'name' => 'Squat',
+        
+        ];
+            
+        DB::table('types')->insert($param);
+        
+        $param = [
+            'name' => 'Deadlift',
+        
+        ];
+            
+        DB::table('types')->insert($param);
+        
+        $param = [
+            'name' => 'Dumbbell fly',
+        
+        ];
+            
+        DB::table('types')->insert($param);
+        
+        $param = [
+            'name' => 'Arm curl',
+        
+        ];
+            
+        DB::table('types')->insert($param);
+        
+        $param = [
+            'name' => 'Push up',
+        
+        ];
+            
+        DB::table('types')->insert($param);
+        
     }
 }
