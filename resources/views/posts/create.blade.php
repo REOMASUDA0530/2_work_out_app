58eec4f6eccfcd6f50eb27e3ea291ffe63c1292c
REOMASUDA0530 <qazsedc0530@gmail.com>
2022-11-13 01:39:47 +0900

    events cannot send

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <body onload='proc();'>
-    <form action="/posts" method="POST">
+    <form action="/posts" method="POST" name='form'>
         @csrf
         <div class='title'>
             <h2>TITLE</h2>
@@ -15,10 +15,7 @@
         </div>
         
         <div class='events'>
-            
-            {{--eventsを作る--}}
-            {{--event[name]の入力--}}
-            
+        
             <div class='add_event'>
                 <h2>ADD YOUR TRAINING</h2>
                 <select name='event[name]' id='event_name'>
@@ -30,38 +27,42 @@
                 <input type='number' name='event[reps]' placeholder='REPS' id='event_reps'>
                 <input type='number' name='event[sets]' placeholder='SETS' id='event_sets'>
                 
-                {{--<input type='button' value='ADD' onclick="show(); add(); ">--}}
+                <input type='button' value='ADD' onclick="show(); add();">
             </div> 
             
-            {{--<div class='added_events'>
+            <div class='added_events'>
                 <h2>YOUR TRAINING</h2>
-                <p id='event_list'></p>
+                <p id='event_value'></p>
                 
-                {{--JavaScriptを書く--
-            </div>--}}
+            </div>
             
         </div>
         
         <div class='user_id'>
             <input type='hidden' name='post[user_id]' value={{ Auth::id() }}>
         </div>
+        
+        
         {{--events 配列をコントローラに渡したい--}}
-        <input type="submit" value="SUBMIT"/>
+        
+        
+        <input type="submit" value="SUBMIT" id='submit' onclick='input_events();'/>
     </form>
     <div class="back">
         <a href="/">BACK</a>
     </div>
     
     <script>
+        
         function show(){
             {{--ADD YOUR TRAINING への表示--}}
-            var event_list = document.getElementById("event_list");
+            var event_value = document.getElementById("event_value");
             var nameValue = document.getElementById('event_name').value;
             var weightValue = document.getElementById('event_weight').value;
             var repsValue = document.getElementById('event_reps').value;
             var setsValue = document.getElementById('event_sets').value;
-            var showValue = nameValue + '×' + weightValue + '×' + repsValue + '×' + setsValue;
-            event_list.insertAdjacentHTML('afterbegin', showValue);
+            var showValue = nameValue + ' : ' + weightValue + 'kg' + '×' + repsValue + 'reps' + '×' + setsValue + 'sets';
+            event_value.insertAdjacentHTML('afterbegin', showValue + '<br>');
             
         }
     
@@ -73,12 +74,18 @@
             event['reps'] = document.getElementById('event_reps').value;
             event['sets'] = document.getElementById('event_sets').value;
             
-            events.push(event);
+            events.unshift(event);
+            
         }
         
         function proc(){
-            {{--events配列の作成--}}
-            var events = [];
+            events = [];
+            
+        }
+        
+        function input_events(){
+            added_events.appendChild(<input type='hidden' name='events[]' value='events'>)
+            
         }
         
     </script>
