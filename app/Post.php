7faec380e0506d82f657a5580dd36d369767e877
REOMASUDA0530 <qazsedc0530@gmail.com>
2022-12-03 03:22:49 +0900

    change css

--- app/Post.php
+++ app/Post.php
@@ -32,7 +32,7 @@ class Post extends Model
     
     public function getPaginateByLimit(int $limit_count = 10)
     {
-        return $this::with('user') ->orderBy('updated_at', 'DESC') -> paginate($limit_count);
+        return $this::with('user') -> orderBy('updated_at', 'DESC') -> paginate($limit_count);
     }
     
     public function getByPost()
